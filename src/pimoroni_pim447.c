2461f25787611958006c430f1ccc778fed39f9a6
asiakobo <syrohei+2@gmail.com>
2025-05-14 14:29:18 +0000

    update

--- src/pimoroni_pim447.c
+++ src/pimoroni_pim447.c
@@ -452,7 +452,7 @@ static int pimoroni_pim447_enable(const struct device *dev)
         LOG_ERR("Failed to configure interrupt GPIO");
         return ret;
     }
-    LOG_DBG("Configured GPIO pin: %d, %d", &config->int_gpio.pin, BIT(config->int_gpio.pin));
+    LOG_DBG("Configured GPIO pin: %d, bitmask: 0x%08X", config->int_gpio.pin, BIT(config->int_gpio.pin));
 
     /* Configure the GPIO interrupt for both edge (active low) */
     ret = gpio_pin_interrupt_configure_dt(&config->int_gpio, GPIO_INT_EDGE_BOTH);
